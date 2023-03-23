0a2f5ecc75b2efc59b64ccc3e2c4c45e798ab219
Sandra <nannan.tian@shihengtech.com>
2023-03-23 17:32:15 +0800

    feat: 地图组件：基本结构完善

---
+++ src/IMap/index.md
@@ -0,0 +1,21 @@
+# IMap
+
+地图
+
+### 何时使用
+
+- 地址定位
+
+### 参考业务场景
+
+- 搜索定位目标地址
+
+### 代码演示
+
+```jsx
+import { IMap } from 'sandra-components';
+
+const App: React.FC = () => <IMap />;
+
+export default App;
+```
--- src/IMap/index.tsx
+++ src/IMap/index.tsx
@@ -1,9 +1,46 @@
+import { APILoader, Map, Marker } from '@uiw/react-amap';
+import { Select } from 'antd';
 import React, { type FC } from 'react';
 
-const IMap: FC = () => {
-  // const map = new AMap.map('container');
+import './index.less';
 
-  return <div id="container">待定</div>;
+const IMap: FC = () => {
+  //  https://restapi.amap.com/v3/assistant/inputtips?parameters?key=d079d062b4ad200ef5cd0117b00c451b&keywords=${''}
+  const onSearch = (value: string) => {
+    console.log(value, '--value');
+  };
+
+  const InnerMap = () => {
+    /**
+     * zoom?:
+     * 	地图显示的缩放级别，可以设置为浮点数；若center与level未赋值，地图初始化默认显示用户所在城市范围。
+     */
+    return (
+      <div className="map-wrap">
+        <Map zoom={14}>
+          <Marker
+            visiable={true}
+            title="上海市"
+            position={new AMap.LngLat(121.472644, 31.231706)}
+          />
+        </Map>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <Select
+        showSearch
+        className="search-wrap"
+        placeholder="请输入关键词搜索"
+        onSearch={onSearch}
+      />
+      <APILoader akay="d079d062b4ad200ef5cd0117b00c451b">
+        <InnerMap />
+      </APILoader>
+    </div>
+  );
 };
 
 export default IMap;
